4a0ba12c2afcde00e167e1f93b576f4ab37400ed
Danilo Souza <danilo_souza19@outlook.com>
2021-04-30 17:04:46 -0300

    first commit

---
+++ src/components/Board.jsx
@@ -0,0 +1,66 @@
+import { useState, useCallback } from 'react';
+import './Board.css'
+import Square from './Square'
+
+export default function Board() {
+  const status = 'Next player: X';
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = useCallback((i) => {
+    const s = squares.slice();
+
+    s[i] = 'X';
+
+    setSquares(s)
+  }, [squares]);
+
+  return (
+    <div className="board-container">
+      <div className="infoGame">
+        <strong>{status}</strong>
+      </div>
+      <dir className="squares-container">
+        <Square
+        value={squares[0]}
+        onClick={() => handleClick(0)}
+      />
+        <Square
+        value={squares[1]}
+        onClick={() => handleClick(1)}
+      />
+        <Square
+        value={squares[2]}
+        onClick={() => handleClick(2)}
+      />
+      </dir>
+      <dir className="squares-container">
+        <Square
+        value={squares[3]}
+        onClick={() => handleClick(3)}
+      />
+        <Square
+        value={squares[4]}
+        onClick={() => handleClick(4)}
+      />
+        <Square
+        value={squares[5]}
+        onClick={() => handleClick(5)}
+      />
+      </dir>
+      <dir className="squares-container">
+        <Square
+        value={squares[6]}
+        onClick={() => handleClick(6)}
+      />
+        <Square
+        value={squares[7]}
+        onClick={() => handleClick(7)}
+      />
+        <Square
+        value={squares[8]}
+        onClick={() => handleClick(8)}
+      />
+      </dir>
+    </div>
+  )
+}
